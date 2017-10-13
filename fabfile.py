6887dec340f11d08840c5d0a71c067b57afbbb49
Alex Morozov <alex@morozov.ca>
2017-10-13 08:43:17 +0000

    Deploy script.

--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,15 @@ from kupola.fabric_class import DjangoFabric,\
 
 
 class Fabric(DjangoFabric):
-    host = 'localhost'
+    host = 'vm4'
+    remote_base_path = '/home/inductor/.virtualenvs/'
+    remote_project_path = '/home/inductor/.virtualenvs/books/'
+    user = 'inductor'
     app_name = 'career_books'
-    repository = 'git@repo.kupo.la:kupola/career_books.git'
-    remote_db_name = 'career_books'
+    repository = 'git@github.com:alexmorozov/books.git'
+    remote_db_name = 'books'
     local_db_name = 'books'
-    use_bower = True
+    use_bower = False
 
 
 __all__ = add_class_methods_as_functions(Fabric(), __name__)
